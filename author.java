747b8aa7df37ae756aabad9756da358a9694168d
Rolay <vinny.sihombing@yahoo.com>
2020-06-22 02:55:48 +0700

    commit

--- author.java
+++ author.java
@@ -3,3 +3,9 @@ package lambdasinaction.appa;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+@Repeatable(Authors.class)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Author {
+
+    String name();
+}
