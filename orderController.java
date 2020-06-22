c99fcc4d6e74bfdb4394fb473b8dc8849321959e
Fitri <38976894+FitrianiGitana3@users.noreply.github.com>
2020-06-22 09:05:10 +0700

    order

---
+++ orderController.java
@@ -0,0 +1,32 @@
+package com.example.ppw.controller;
+//
+import com.example.ppw.model.orders;
+import com.example.ppw.repository.ordersRepo;
+import com.example.ppw.repository.testNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+//fungsi
+//untuk
+/controller
+@RestController
+@RequestMapping("/shoppingcart")
+public class ContohKondisi {
+
+    public static void main(String[] args) {
+        boolean kondisi;
+
+        kondisi= (1+1) == 2 && 2>1;
+
+        if (kondisi){
+            System.out.println("Jawaban anda benar...");
+        }
+        else {
+            System.out.println("Jawaban anda benar salah...");
+        }
+    }
+}
